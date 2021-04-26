8b25f228c9a5104ca6d72d6cfbe07cdb0cf8ee17
tbroed <tbroed@web.de>
2021-04-26 17:30:27 +0200

    added init_visualizer() and draw_registration_result() to display_point_cloud.py

--- python/tools/display_point_cloud.py
+++ python/tools/display_point_cloud.py
@@ -2,10 +2,38 @@ import open3d as open3d
 import numpy as np
 import os
 import sys
+import copy
 sys.path.insert(0, '')
 
 from transform import build_se3_transform
 
+
+def init_visualizer_dpc():
+    vis = open3d.visualization.Visualizer()
+    vis.create_window(window_name=os.path.basename(__file__))
+    render_option = vis.get_render_option()
+    render_option.background_color = np.array([0.1529, 0.1569, 0.1333], np.float32)
+    render_option.point_color_option = open3d.visualization.PointColorOption.Default
+    return vis
+
+
+def draw_registration_result_dpc(source, target, transformation):
+    vis = init_visualizer_dpc()
+
+    source_temp = copy.deepcopy(source)
+    target_temp = copy.deepcopy(target)
+    source_temp.paint_uniform_color([1, 0.706, 0])
+    target_temp.paint_uniform_color([0, 0.651, 0.929])
+    source_temp.transform(transformation)
+    # vis.draw_geometries([source_temp, target_temp],
+    #                                   zoom=0.4459,
+    #                                   front=[0.9288, -0.2951, -0.2242],
+    #                                   lookat=[1.6784, 2.0612, 1.4451],
+    #                                   up=[-0.3402, -0.9189, -0.1996])
+    vis.add_geometry(source_temp)
+    vis.add_geometry(target_temp)
+    vis.run()
+    vis.close()
 
 print("Load a ply point cloud, print it, and render it")
 # pcd = open3d.io.read_point_cloud("output/Point_Clouds/pc_all_gps.ply")
@@ -14,15 +42,15 @@ print("Load a ply point cloud, print it, and render it")
 #show that ICP works
 #show that vo itself is already really good (del -target_num)
 # pcd = open3d.io.read_point_cloud("output/Point_Clouds/pc_all_gps.ply")
-# draw_registration_result(open3d.io.read_point_cloud("output/Point_Clouds/pc_all_vo.ply"), open3d.io.read_point_cloud("output/Point_Clouds/pc_all_gps.ply"),np.identity(4))
+# draw_registration_result_dpc(open3d.io.read_point_cloud("output/Point_Clouds/pc_all_vo.ply"), open3d.io.read_point_cloud("output/Point_Clouds/pc_all_gps.ply"),np.identity(4))
 # pcd = open3d.io.read_point_cloud("output/Point_Clouds/pc_all_icp_vo.ply")
 # pcd = open3d.io.read_point_cloud("output/Point_Clouds/pc_all_icp_only_p2plane.ply")
-# draw_registration_result(open3d.io.read_point_cloud("output/Point_Clouds/pc_all_vo.ply"), open3d.io.read_point_cloud("output/Point_Clouds/pc_all_icp_vo.ply"),np.identity(4))
+# draw_registration_result_dpc(open3d.io.read_point_cloud("output/Point_Clouds/pc_all_vo.ply"), open3d.io.read_point_cloud("output/Point_Clouds/pc_all_icp_vo.ply"),np.identity(4))
 # pcd = open3d.io.read_point_cloud("output/Point_Clouds/RaLL_robotcar_map.ply")
 
 # Neu:
-pcd = open3d.io.read_point_cloud("output/Point_Clouds/pc_icp_every_10_frame.ply")
-# draw_registration_result(open3d.io.read_point_cloud("output/Point_Clouds/pc_all_vo.ply"), open3d.io.read_point_cloud("output/Point_Clouds/pc_icp_every_10_frame.ply"),np.identity(4))
+# pcd = open3d.io.read_point_cloud("output/Point_Clouds/pc_g2o_stirde_5.ply")
+# draw_registration_result_dpc(open3d.io.read_point_cloud("output/Point_Clouds/pc_g2o_before_stride_4_long.ply"), open3d.io.read_point_cloud("output/Point_Clouds/pc_g2o_stride_4_long.ply"),np.identity(4))
 # pcd = open3d.io.read_point_cloud("output/Point_Clouds/pc_icp_200_400_every_10th.ply")
 
 
